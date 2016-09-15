cb17c81ad345bb3a6af02be5b05f93cb2a9b5b3e
Diego Angulo <gus0020@gmail.com>
2016-09-15 14:32:39 -0500

    test aot

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>My First Angular 2 App</h1>
+     <h2>{{my_date | date:'medium'}}</h2>`
 })
-export class AppComponent { }
+export class AppComponent {
+  my_date = "2016-09-05T14:30:45Z";
+ }
